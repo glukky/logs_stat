a848d0da964c620e566a04808516b6de42808d24
glukky <blackmamba05@yandex.ru>
2021-05-06 11:26:03 +0300

    add pretty output

--- logs_counter.py
+++ logs_counter.py
@@ -11,9 +11,22 @@ def main():
             match = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', str_a)
             if match:
                 ip = match[0]
-                stat.setdefault(ip, 0)
-                stat[ip] += 1
+                stat.setdefault(ip, {})
+                match = re.search(r'" (\d{3}) ', str_a)
+                if match:
+                    code = match[1]
+                    stat[ip].setdefault(code, 0)
+                    stat[ip][code] += 1
 
-        print(stat)
+
+        for k, v in stat.items():
+            print(k)
+            codes = sorted(
+                [(code, count) for code, count in v.items()],
+                key=lambda x: x[1],
+                reverse=True,
+            )
+            for code, count in codes:
+                print('    ', code, count)
 if __name__ == main():
     main()
